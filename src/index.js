744474a4713b6186eeef2127abf0273abffc7d4d
Falk <petruk0908@gmail.com>
2020-02-04 22:35:27 +0300

    add materialize

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'materialize-css/dist/js/materialize.js';
+import 'materialize-css/dist/css/materialize.css';
 import './index.css';
 import ReactTable from './ReactTable.jsx';
 import * as serviceWorker from './serviceWorker';
