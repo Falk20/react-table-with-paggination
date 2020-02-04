3be9517f06102b4337e1de8a76c4bfc5ca050c31
Falk <petruk0908@gmail.com>
2020-02-04 23:25:35 +0300

    add scss support

--- src/components/DataCountChanger.jsx
+++ src/components/DataCountChanger.jsx
@@ -22,10 +22,10 @@ export default class DataCountChanger extends Component {
 
     render() {
         return (
-            <div className="data-count">
-                <label>Сколько загрузить?</label>
-                <button onClick={this.clickHandle} value='32'>32 записи</button>
-                <button onClick={this.clickHandle} value='1000'>1000 записей</button>
+            <div className='data-count'>
+                <span>Сколько загрузить?</span>
+                <button className='waves-effect waves-light btn-small' onClick={this.clickHandle} value='32'>32 записи</button>
+                <button className='waves-effect waves-light btn-small' onClick={this.clickHandle} value='1000'>1000 записей</button>
             </div>
         )
     }
