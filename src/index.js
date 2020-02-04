0e017dcca38aeec724fd21c9ce728c02cd9c7a3f
Falk <petruk0908@gmail.com>
2020-02-04 21:20:41 +0300

    fix import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ReactTable from './ReactTable';
+import ReactTable from './ReactTable.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<ReactTable />, document.getElementById('root'));
