e884235d8fa11886a422272d7faf62ffd32b3349
Falk <petruk0908@gmail.com>
2020-02-04 21:24:51 +0300

    add fetch records

---
+++ src/components/DataCountChanger.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class DataCountChanger extends Component {
+    clickHandle = (e) => {
+        let url = new URL('http://www.filltext.com/');
+        url.searchParams.append('rows', e.target.value);
+        url.searchParams.append('id', '{number|1000}');
+        url.searchParams.append('firstName', '{firstName}');
+        url.searchParams.append('lastName', '{lastName}');
+        url.searchParams.append('email', '{email}');
+        url.searchParams.append('phone', '{phone|(xxx)xxx-xx-xx}');
+        url.searchParams.append('address', '{addressObject}');
+        url.searchParams.append('description', '{lorem|32}');
+
+        fetch(url)
+            .then(res => res.json())
+            .then(result => {
+                this.props.changeCount(result);
+            })
+            .catch(console.log);
+    }
+
+    render() {
+        return (
+            <div className="data-count">
+                <label>Сколько загрузить?</label>
+                <button onClick={this.clickHandle} value='32'>32 записи</button>
+                <button onClick={this.clickHandle} value='1000'>1000 записей</button>
+            </div>
+        )
+    }
+}
