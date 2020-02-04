e582a44c08e6f81a7280696ea0c2ee7798cecc20
Falk <petruk0908@gmail.com>
2020-02-04 20:42:10 +0300

    rename main component

---
+++ src/ReactTable.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ReactTable extends Component {
+  render() {
+    return (
+      <div className="">
+        
+      </div>
+    )
+  }
+}
