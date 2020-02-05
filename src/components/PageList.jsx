41fd561be819106a7f9d7293254f44174c0525fe
Falk <petruk0908@gmail.com>
2020-02-05 03:13:23 +0300

    add pageChange

--- src/components/PageList.jsx
+++ src/components/PageList.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
 export default class PageList extends Component {
+    clickHandle = (e) => {
+        this.props.changePage(+e.target.dataset.page);
+    }
+
     render() {
         let pages = [];
 
         for (let i = 0; i < this.props.pageCount; i++) {
             pages[i] = (
-                <a className={i === this.props.currentPage ? 'current' : ''}>{i+1}</a>
+                <a
+                    key={i}
+                    className={i === this.props.currentPage ? 'current' : ''}
+                    data-page={i}
+                    onClick={this.clickHandle}
+                >
+                    {i + 1}
+                </a>
             )
         }
 
