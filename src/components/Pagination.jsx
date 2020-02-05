df3d52b57add65e5ea9099d39feb636abcd25979
Falk <petruk0908@gmail.com>
2020-02-05 03:04:06 +0300

    pagination implemented

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PageList from './PageList';
+
+export default class Pagination extends Component {
+    clickHandle = (e) => {
+        if (e.target.id === 'next-page' && this.props.currentPage < this.props.pageCount - 1) {
+            this.props.changePage(this.props.currentPage + 1);
+        }
+
+        if (e.target.id === 'prev-page' && this.props.currentPage > 0) {
+            this.props.changePage(this.props.currentPage - 1);
+        }
+    }
+
+    render() {
+        return (
+            <div className='pagination'>
+                <a id='prev-page' onClick={this.clickHandle}>{'<'}</a>
+                <PageList
+                    pageCount={this.props.pageCount}
+                    currentPage={this.props.currentPage}
+                />
+                <a id='next-page' onClick={this.clickHandle}>{'>'}</a>
+            </div>
+        )
+    }
+}
