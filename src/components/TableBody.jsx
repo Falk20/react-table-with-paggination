c7c41d0f057037aaa407d625d8e765f5d2863aaf
Falk <petruk0908@gmail.com>
2020-02-05 03:02:55 +0300

    complite TableBody

---
+++ src/components/TableBody.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export default class TableBody extends Component {
+    clickHandle = (e) => {
+        let choosenTr = e.target.closest('tr');
+        let info = {
+            id: choosenTr['id'].innerText
+        };
+
+        console.log(info);
+    }
+
+    render() {
+        let records = this.props.records.map((record) => {
+            let address = record.address;
+            let viewAddress = `${address.state}, ${address.city},\n${address.streetAddress}`;
+            return (
+                <tr key={record.id + record.email}>
+                    <td className="id" name='id'>{record.id}</td>
+                    <td className="first-name" name="first-name">{record.firstName}</td>
+                    <td className="last-name" name="last-name">{record.lastName}</td>
+                    <td className="email" name="email">{record.email}</td>
+                    <td className="phone" name="phone">{record.phone}</td>
+                    <td className="address" name="address">{viewAddress}</td>
+                    <td className="description" name="description" title={record.description}>{record.description}</td>
+                </tr>
+            )
+        });
+
+        return (
+            <tbody onClick={this.clickHandle}>
+                {records}
+            </tbody>
+        )
+    }
+}
