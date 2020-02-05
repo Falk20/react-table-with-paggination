ed6abbb2753a5699b5dd13d9dec0f5d762746afc
Falk <petruk0908@gmail.com>
2020-02-05 17:00:46 +0300

    add button for show Form

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -3,11 +3,16 @@ import RecordsCount from './RecordsCount';
 import Filter from './Filter';
 
 export default class MainHeader extends Component {
+    addRecordHandle = (e) => {
+        document.querySelector('.form-wrapper').classList.remove('hidden');
+    }
+
     render() {
         return (
             <nav className=''>
                 <h1>Таблица</h1>
                 <RecordsCount changeCount={this.props.changeCount}/>
+                <button className="add-new-record" onClick={this.addRecordHandle}>Новая запись</button>
                 <Filter filterRecords={this.props.filterRecords}/>
             </nav>
         )
