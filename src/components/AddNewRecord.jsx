6f2ddef0b32d9015a08561827a98fdfd43c1984b
Falk <petruk0908@gmail.com>
2020-02-05 17:01:06 +0300

    add submit handler

--- src/components/AddNewRecord.jsx
+++ src/components/AddNewRecord.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
 
 export default class AddNewRecord extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+
+    submitHandle = (e) => {
+        e.preventDefault();
+        let formElems = e.target.elements;
+        this.setState({
+            id: formElems['id'].value,
+            firstName: formElems['first-name'].value,
+            lastName: formElems['last-name'].value,
+            email: formElems['email'].value,
+            phone: formElems['phone'].value,
+            address: {
+                streetAddress: formElems['street-address'].value,
+                city: formElems['city'].value,
+                state: formElems['state'].value,
+                zip: formElems['zip'].value,
+            },
+            description: formElems['description'].value
+        }, ()=> {
+            this.props.addNewRecord(this.state);
+        });
+    }
+
+    clickHandle = (e) => {
+        if (e.target.classList.contains('form-wrapper') || e.target.classList.contains('close-form') || e.target.classList.contains('send-form')) {
+            e.target.closest('.form-wrapper').classList.add('hidden');
+        }
+    }
+
     render() {
         return (
-            <div className='form-wrapper'>
-                <form className="add-new-record">
+            <div className='form-wrapper hidden' onClick={this.clickHandle}>
+                <form className="add-new-record" onSubmit={this.submitHandle}>
                     <label htmlFor="id">ID:</label>
                     <input id='id' name='id' type="number" />
                     <label htmlFor="first-name">First Name:</label>
@@ -26,7 +62,7 @@ export default class AddNewRecord extends Component {
                     <label htmlFor="description">Description:</label>
                     <textarea id='description' name='description' />
 
-                    <button type="submit">Save</button>
+                    <button className='send-form' type="submit">Save</button>
                     <span className="close-form">x</span>
                 </form>
             </div>
