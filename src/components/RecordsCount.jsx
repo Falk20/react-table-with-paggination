04f3d9362a4e952fd2b5c37896ea523de1a23aa0
Falk <petruk0908@gmail.com>
2020-02-05 05:36:28 +0300

    add spinner active

--- src/components/RecordsCount.jsx
+++ src/components/RecordsCount.jsx
@@ -11,21 +11,24 @@ export default class RecordsCount extends Component {
         url.searchParams.append('phone', '{phone|(xxx)xxx-xx-xx}');
         url.searchParams.append('address', '{addressObject}');
         url.searchParams.append('description', '{lorem|32}');
-
+        let spinner = document.querySelector('#spinner')
+        spinner.classList.add('show');
         fetch(url)
             .then(res => res.json())
             .then(result => {
                 this.props.changeCount(result);
+                spinner.classList.remove('show');
             })
             .catch(console.log);
     }
-    
+
     render() {
         return (
             <div className=''>
                 <ul className=''>
                     <li className=''><button onClick={this.clickHandle} data-value='32'>32 записи</button></li>
                     <li className=''><button onClick={this.clickHandle} data-value='1000'>1000 записей</button></li>
+                    <li><div id="spinner"></div></li>
                 </ul>
             </div>
         )
