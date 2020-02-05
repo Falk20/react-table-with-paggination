cb930784d6a88e3f2b8912da7701f31d57077778
Falk <petruk0908@gmail.com>
2020-02-05 04:38:21 +0300

    change " to '

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -16,13 +16,13 @@ export default class TableBody extends Component {
             let viewAddress = `${address.state}, ${address.city},\n${address.streetAddress}`;
             return (
                 <tr key={record.id + record.email}>
-                    <td className="id" name='id'>{record.id}</td>
-                    <td className="first-name" name="first-name">{record.firstName}</td>
-                    <td className="last-name" name="last-name">{record.lastName}</td>
-                    <td className="email" name="email">{record.email}</td>
-                    <td className="phone" name="phone">{record.phone}</td>
-                    <td className="address" name="address">{viewAddress}</td>
-                    <td className="description" name="description" title={record.description}>{record.description}</td>
+                    <td className='id' name='id'>{record.id}</td>
+                    <td className='first-name' name='first-name'>{record.firstName}</td>
+                    <td className='last-name' name='last-name'>{record.lastName}</td>
+                    <td className='email' name='email'>{record.email}</td>
+                    <td className='phone' name='phone'>{record.phone}</td>
+                    <td className='address' name='address'>{viewAddress}</td>
+                    <td className='description' name='description' title={record.description}>{record.description}</td>
                 </tr>
             )
         });
